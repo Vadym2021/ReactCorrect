311e2dc77398a6ea3399f5762e191673ae7004bb
Vadym2021 <aisclass@ukr.net>
2022-01-10 20:55:16 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from "./Components/Homework 1";
+
+function App() {
+    return (
+        <div className="App">
+
+
+
+        </div>
+    );
+}
+
+export default App;
